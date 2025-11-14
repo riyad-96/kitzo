fc45e95cae16db575dca66a5c11247ca548d6116
riyad-96 <riyadofficial.ide@gmail.com>
2025-11-14 21:29:26 +0600

    build successful

--- dist/react.esm.js
+++ dist/react.esm.js
@@ -185,23 +185,23 @@ function toastStyles() {
 `;
 }
 const listeners = new Set();
-let isStyleAdded = false;
+let isStyleAdded$1 = false;
 function addToast(toast) {
   listeners.forEach(callback => {
     callback(toast);
   });
 }
 function subscribe(callback) {
-  if (!isStyleAdded) {
+  if (!isStyleAdded$1) {
     const styleTag = document.createElement('style');
     styleTag.textContent = toastStyles();
     document.head.appendChild(styleTag);
-    isStyleAdded = true;
+    isStyleAdded$1 = true;
   }
   listeners.add(callback);
   return () => listeners.delete(callback);
 }
-function success(text = 'Toast success', options = {}) {
+function toast(text = 'Toast message', options = {}) {
   const id = Date.now();
   options = Object.assign({
     duration: 2000,
@@ -215,7 +215,21 @@ function success(text = 'Toast success', options = {}) {
     options
   });
 }
-function error(text = 'Toast denied', options = {}) {
+toast.success = (text = 'Toast success', options = {}) => {
+  const id = Date.now();
+  options = Object.assign({
+    duration: 2000,
+    id,
+    style: {},
+    showIcon: true
+  }, options);
+  addToast({
+    type: 'success',
+    text,
+    options
+  });
+};
+toast.error = (text = 'Toast denied', options = {}) => {
   const id = Date.now();
   options = Object.assign({
     duration: 2000,
@@ -228,8 +242,8 @@ function error(text = 'Toast denied', options = {}) {
     text,
     options
   });
-}
-function promise(callback, msgs = {}, options = {}) {
+};
+toast.promise = (callback, msgs = {}, options = {}) => {
   const id = Date.now();
   options = Object.assign({
     duration: 2000,
@@ -269,8 +283,8 @@ function promise(callback, msgs = {}, options = {}) {
     });
     return Promise.reject(normalizedError);
   });
-}
-function custom(render, options = {}) {
+};
+toast.custom = (render, options = {}) => {
   const id = Date.now();
   options = Object.assign({
     duration: 3000,
@@ -283,7 +297,7 @@ function custom(render, options = {}) {
     options
   });
   return id;
-}
+};
 function dismiss(id, exitDelay) {
   addToast({
     type: 'dismiss',
@@ -541,11 +555,138 @@ function ToastContainer(props) {
   }));
 }
 
-const toast = {
-  success,
-  error,
-  promise,
-  custom
-};
+const tooltipStyles = `
+.kitzo-react-tooltip-content-default-style {
+  font-family: sans-serif;
+  font-size: 0.875rem;
+  background-color: hsl(0, 0%, 15%);
+  color: hsl(0, 0%, 95%);
+  padding-block: 8px;
+  padding-inline: 10px;
+  border-radius: 0.325rem;
 
-export { ToastContainer, toast };
+  @media (prefers-color-scheme: dark) {
+    background-color: hsl(0, 0%, 95%);
+    color: hsl(0, 0%, 15%);
+  }
+}
+
+.kitzo-react-tooltip-wrapper {
+  transition-duration: 120ms, 50ms;
+  transition-property: scale opacity;
+}
+
+.kitzo-react-tooltip-wrapper.top {
+  --tooltip-offset: calc(var(--offset) * 1px);
+
+  bottom: calc(var(--tooltip-offset) + 100%);
+  left: 50%;
+  translate: -50% 0;
+  scale: 0.7;
+  opacity: 0;
+  transform-origin: bottom;
+}
+
+.kitzo-react-tooltip-wrapper.right {
+  --tooltip-offset: calc(var(--offset) * 1px);
+
+  left: calc(var(--tooltip-offset) + 100%);
+  top: 50%;
+  translate: 0 -50%;
+  scale: 0.7;
+  opacity: 0;
+  transform-origin: left;
+}
+
+.kitzo-react-tooltip-wrapper.bottom {
+  --tooltip-offset: calc(var(--offset) * 1px);
+
+  top: calc(var(--tooltip-offset) + 100%);
+  left: 50%;
+  translate: -50% 0;
+  scale: 0.7;
+  opacity: 0;
+  transform-origin: top;
+}
+
+.kitzo-react-tooltip-wrapper.left {
+  --tooltip-offset: calc(var(--offset) * 1px);
+
+  right: calc(var(--tooltip-offset) + 100%);
+  top: 50%;
+  translate: 0 -50%;
+  scale: 0.7;
+  opacity: 0;
+  transform-origin: right;
+}
+
+.kitzo-react-tooltip-root:hover {
+  .kitzo-react-tooltip-wrapper.top {
+    scale: 1;
+    opacity: 1;
+  }
+  .kitzo-react-tooltip-wrapper.right {
+    scale: 1;
+    opacity: 1;
+  }
+  .kitzo-react-tooltip-wrapper.bottom {
+    scale: 1;
+    opacity: 1;
+  }
+  .kitzo-react-tooltip-wrapper.left {
+    scale: 1;
+    opacity: 1;
+  }
+}
+`;
+(() => {
+  if (!document.getElementById('kitzo-react-tooltip-styles')) {
+    const styleTag = document.createElement('style');
+    styleTag.id = 'kitzo-react-tooltip-styles';
+    styleTag.textContent = tooltipStyles;
+    document.head.appendChild(styleTag);
+    isStyleAdded = true;
+  }
+})();
+function getPositionBasedClassName(position) {
+  let defaultClass = 'kitzo-react-tooltip-wrapper';
+  const allowedPositions = ['top', 'right', 'bottom', 'left'];
+  if (allowedPositions.includes(position)) {
+    return defaultClass + ' ' + position;
+  } else {
+    return defaultClass + ' ' + 'top';
+  }
+}
+function Tooltip({
+  content = 'Tooltip',
+  position = '',
+  offset = 8,
+  children
+}) {
+  // sanitize props
+  if (typeof position !== 'string') {
+    console.warn(`[kitzo/react]: Tooltip position ignored due to invalid data type.`);
+    position = 'top';
+  }
+  if (isNaN(Number(offset))) offset = 8;
+  return /*#__PURE__*/React.createElement("div", {
+    style: {
+      position: 'relative',
+      width: 'fit-content',
+      '--offset': Math.max(0, offset)
+    },
+    className: "kitzo-react-tooltip-root"
+  }, children, /*#__PURE__*/React.createElement("div", {
+    style: {
+      position: 'absolute',
+      textWrap: 'nowrap',
+      pointerEvents: 'none'
+    },
+    tabIndex: -1,
+    className: getPositionBasedClassName(position.trim().toLowerCase())
+  }, typeof content === 'string' || typeof content === 'number' ? /*#__PURE__*/React.createElement("div", {
+    className: "kitzo-react-tooltip-content-default-style"
+  }, content) : /*#__PURE__*/React.createElement(React.Fragment, null, content)));
+}
+
+export { ToastContainer, Tooltip, toast };
