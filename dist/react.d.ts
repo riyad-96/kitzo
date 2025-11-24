bac7c315dc16194506afe0a6f4e648ce1d921d15
riyad-96 <riyadofficial.ide@gmail.com>
2025-11-24 12:28:05 +0600

    minor changes in toast api types

--- dist/react.d.ts
+++ dist/react.d.ts
@@ -1,8 +1,10 @@
-import { type ReactNode, type CSSProperties, type JSX, type PropsWithChildren } from 'react';
+import type { ReactNode, CSSProperties, JSX, PropsWithChildren } from 'react';
 
-// Toast API types declaration
+//! Toast API types declaration
+export type Positions = 'top-left' | 'top-center' | 'top-right' | 'bottom-left' | 'bottom-center' | 'bottom-right';
+
 export interface ToastOptions {
-  position?: 'top-left' | 'top-center' | 'top-right' | 'bottom-left' | 'bottom-center' | 'bottom-right';
+  position?: Positions;
   duration?: number;
   style?: CSSProperties;
   showIcon?: boolean;
@@ -12,25 +14,29 @@ export type CustomContent = string | ReactNode | ((dismiss: () => void) => React
 
 export interface ToastAPI {
   (text?: string, options?: ToastOptions): void;
+
   success(text?: string, options?: ToastOptions): void;
+
   error(text?: string, options?: ToastOptions): void;
+
   promise<T>(
     callback: Promise<T>,
     msgs?: {
       loading?: string;
-      success?: string | ((res: T) => string);
-      error?: string | ((err: Error) => string);
+      success?: string | ((res: T) => string | Promise<string>);
+      error?: string | ((err: any) => string | Promise<string>);
     },
     options?: ToastOptions,
-  ): Promise<T>;
-  custom(content: CustomContent, options?: { duration?: number; exitDelay?: number; position?: 'top-left' | 'top-center' | 'top-right' | 'bottom-left' | 'bottom-center' | 'bottom-right' }): void;
+  ): void;
+
+  custom(content: CustomContent, options?: { duration?: number; exitDelay?: number; position?: Positions }): void;
 }
 
 export declare const toast: ToastAPI;
 
-export declare function ToastContainer(props: { position?: 'top-left' | 'top-center' | 'top-right' | 'bottom-left' | 'bottom-center' | 'bottom-right'; gap?: number }): JSX.Element;
+export declare function ToastContainer(props: { position?: Positions; gap?: number }): JSX.Element;
 
-// Tooltip API types declaration
+//! Tooltip API types declaration
 export interface TooltipCoreProps {
   content: string | ReactNode;
   position?: 'top' | 'right' | 'bottom' | 'left';
