c5394b2ac476518029eeeb8371b8b4aaed26879e
riyad-96 <riyadofficial.ide@gmail.com>
2026-01-12 20:53:32 +0600

    fixed dismiss ghosting issue

--- dist/react/components/toast/helpers/manageToasts/dismissToasts.js
+++ dist/react/components/toast/helpers/manageToasts/dismissToasts.js
@@ -1,20 +1,19 @@
-import { LEAVE_DELAY as l, clearAllTimers as d, clearTimer as f } from "./timers.js";
-function m({ toast: t, setToasts: n }) {
-  if (!t.id) {
-    let i = 0;
-    if (n((e) => (i = e.length, e.length === 0 ? e : e.map((r) => ({ ...r, status: "leaving" })))), i === 0) return;
-    setTimeout(() => {
-      n([]);
-    }, l), d();
+import { LEAVE_DELAY as a, clearAllTimers as m, clearTimer as u } from "./timers.js";
+function n({ toast: l, setToasts: r }) {
+  if (!l.id) {
+    r((e) => e.map((i) => ({ ...i, status: "leaving" }))), setTimeout(() => {
+      r((e) => e.filter((i) => i.status !== "leaving"));
+    }, a), m();
     return;
   }
-  let u = !0;
-  n((i) => i.find((r) => r.id === t.id) ? i.map(
-    (r) => r.id === t.id ? { ...r, status: "leaving" } : r
-  ) : (u = !1, i)), u && (f(t.id), setTimeout(() => {
-    n((i) => i.filter((e) => e.id !== t.id));
-  }, l));
+  u(l.id), r(
+    (e) => e.map((i) => i.id === l.id ? { ...i, status: "leaving" } : i)
+  ), setTimeout(() => {
+    r(
+      (e) => e.filter((i) => !(i.id === l.id && i.status === "leaving"))
+    );
+  }, a);
 }
 export {
-  m as default
+  n as default
 };
--- src/lib/react/components/toast/helpers/manageToasts/dismissToasts.ts
+++ src/lib/react/components/toast/helpers/manageToasts/dismissToasts.ts
@@ -4,45 +4,28 @@ import { clearAllTimers, clearTimer, LEAVE_DELAY } from './timers';
 export default function dismissToasts({ toast, setToasts }: ManageToastsProps) {
   // clear all toast
   if (!toast.id) {
-    let toastCount = 0;
-
-    setToasts((prev) => {
-      toastCount = prev.length;
-      if (prev.length === 0) return prev;
-      return prev.map((t) => ({ ...t, status: 'leaving' }));
-    });
-
-    if (toastCount === 0) return;
+    setToasts((prev) => prev.map((t) => ({ ...t, status: 'leaving' })));
 
     setTimeout(() => {
-      setToasts([]);
+      setToasts((prev) => prev.filter((t) => t.status !== 'leaving'));
     }, LEAVE_DELAY);
 
     clearAllTimers();
     return;
   }
 
-  // set toast status leaving
-  let doesExists = true;
-
-  setToasts((prev) => {
-    const exists = prev.find((t) => t.id === toast.id);
-    if (!exists) {
-      doesExists = false;
-      return prev;
-    }
-    return prev.map((t) =>
-      t.id === toast.id ? { ...t, status: 'leaving' } : t,
-    );
-  });
-
-  if (!doesExists) return;
-
   // clear timer
   clearTimer(toast.id);
 
+  // set toast status leaving
+  setToasts((prev) =>
+    prev.map((t) => (t.id === toast.id ? { ...t, status: 'leaving' } : t)),
+  );
+
   // remove toast
   setTimeout(() => {
-    setToasts((prev) => prev.filter((t) => t.id !== toast.id));
+    setToasts((prev) =>
+      prev.filter((t) => !(t.id === toast.id && t.status === 'leaving')),
+    );
   }, LEAVE_DELAY);
 }
